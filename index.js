3db9aaafd66aef4728b47cf1a696b5e230bf985e
Nespresso <cmiarg@gmail.com>
2021-09-07 14:56:07 -0300

    se agrega la validacion del token de seguridad para agregar operaciones- se agrega a traves de middware

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 8000;
 };
 
 app.use(cors(corsOptions)); */
-
+var jwt = require('jsonwebtoken');
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
