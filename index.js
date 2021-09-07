66fb78dbc3a745b92dc771d98d15907c9de0e46c
Nespresso <cmiarg@gmail.com>
2021-09-07 14:53:34 -0300

    se agrega la validacion del token de seguridad para agregar operaciones- se agrega a traves de middware

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ require("dotenv").config();
 require("./databases");
 
 const PORT = process.env.PORT || 8000;
-var corsOptions = {
+/* var corsOptions = {
   "origin": ['*'],
   methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -23,10 +23,29 @@ var corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
 };
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)); */
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+  const token=req.headers.authorization
+   var decoded = jwt.decode(token);
+    if (Date.now() >= decoded.exp*1000) {
+        console.log("token expirado")
+        return res.status(401).send({message:'Token expirado'});      
+      }else{
+        console.log("token ok!") 
+        
+        next();
+      }
+  
+});
 app.use(express.json());
 app.use(getTimes)
-app.use(validarTokenFn)
+//app.use(validarTokenFn)
 app.set("port", PORT);
 app.set("views", path.join(__dirname, "views"));
 app.engine(
